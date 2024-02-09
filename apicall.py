4d5de141b9aae1092a368c6809445602d49532ef
Shreya <ekandeshreya9@gmail.com>
2024-02-08 23:21:41 -0500

    API call code

--- apicall.py
+++ apicall.py
@@ -1,17 +1,21 @@
 import requests
 from xml.etree import ElementTree
 
-def perform_esearch(database, retmax):
+def perform_esearch(database, retmax, term):
     base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
     params = {
         "db": database,
-        "retmax": retmax
+        "retmax": retmax,
+        "term": term  # Add search term parameter
     }
     response = requests.get(base_url, params=params)
     if response.status_code == 200:
         return response.text
     else:
+        print("ESearch request failed with status code:", response.status_code)
+        print("Response content:", response.text)
         return None
+
 
 def perform_efetch(database, ids):
     base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
@@ -24,6 +28,8 @@ def perform_efetch(database, ids):
     if response.status_code == 200:
         return response.text
     else:
+        print("EFetch request failed with status code:", response.status_code)
+        print("Response content:", response.text)
         return None
 
 def parse_efetch_response(response):
@@ -41,22 +47,30 @@ def parse_efetch_response(response):
 
 def main():
     # Perform ESearch to retrieve PubMed IDs
-    esearch_response = perform_esearch(database="pubmed", retmax=10)
+    esearch_response = perform_esearch(database="pubmed", retmax=10, term="cancer")
     if esearch_response:
         esearch_root = ElementTree.fromstring(esearch_response)
         id_list = [id_node.text for id_node in esearch_root.findall(".//Id")]
-        # Perform EFetch to retrieve detailed information for PubMed records
-        efetch_response = perform_efetch(database="pubmed", ids=id_list)
-        if efetch_response:
-            # Parse EFetch response
-            records = parse_efetch_response(efetch_response)
-            # Print detailed information for each record
-            for i, record in enumerate(records, start=1):
-                print("Record", i)
-                print("Title:", record.get("Title"))
-                print("Authors:", record.get("Authors"))
-                print("Abstract:", record.get("Abstract"))
-                print()
+        print("PubMed IDs:", id_list)  # Print PubMed IDs obtained from ESearch
+        if id_list:
+            # Perform EFetch to retrieve detailed information for PubMed records
+            efetch_response = perform_efetch(database="pubmed", ids=id_list)
+            if efetch_response:
+                # Parse EFetch response
+                records = parse_efetch_response(efetch_response)
+                # Print detailed information for each record
+                for i, record in enumerate(records, start=1):
+                    print("Record", i)
+                    print("Title:", record.get("Title"))
+                    print("Authors:", record.get("Authors"))
+                    print("Abstract:", record.get("Abstract"))
+                    print()
+            else:
+                print("EFetch response is empty.")
+        else:
+            print("No PubMed IDs obtained from ESearch.")
+    else:
+        print("ESearch response is empty.")
 
 if __name__ == "__main__":
     main()
